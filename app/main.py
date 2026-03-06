197236b36abc0c7c57cf7da47c18791eb69004a3
Suleyman <suleymanmalikov03@gmail.com>
2026-03-06 19:12:13 +0100

    feat: Alembic added to habit tracker

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 
-from app.database.session import engine 
-from app.database.base import Base 
-from app.database import models
 from app.routers import habit, completion
 
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
 
 app.include_router(habit.router)
 app.include_router(completion.router)
