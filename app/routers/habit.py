cfafdb1a84fa406a77e35cee08cf8e0077350a3f
Suleyman <suleymanmalikov03@gmail.com>
2026-03-06 15:34:39 +0100

    feat: create habit_service

--- app/routers/habit.py
+++ app/routers/habit.py
@@ -5,6 +5,7 @@ from typing import List, Annotated
 from app.database.session import get_db 
 from app.database.models import Habit
 from app.schemas.habit import HabitCreate, HabitRead, HabitUpdate
+from app.services import habit_service
 
 router = APIRouter(
     prefix="/habits",
@@ -14,57 +15,21 @@ router = APIRouter(
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.get("/", response_model=List[HabitRead])
-# def get_all_habits(db: Session = Depends(get_db)):
 def get_habits(db: db_dependency):
-    habits = db.query(Habit).order_by(Habit.created_at, Habit.id).all()
-    return habits
+    return habit_service.get_habits(db)
 
 @router.get("/{habit_id}", response_model=HabitRead)
 def get_habit(habit_id: int, db: db_dependency):
-    habit = db.query(Habit).filter(Habit.id == habit_id).first()
-
-    if habit is None:
-        raise HTTPException(status_code=404, detail="Habit not found")
-    
-    return habit
+    return habit_service.get_habit(habit_id, db)
 
 @router.post("/", response_model=HabitRead)
 def create_habit(habit: HabitCreate, db: db_dependency):
-    new_habit = Habit(
-        title = habit.title,
-        description = habit.description
-    )
-
-    db.add(new_habit)
-    db.commit()
-    db.refresh(new_habit)
-
-    return new_habit
+    return habit_service.create_habit(habit, db)
     
 @router.put("/{habit_id}", response_model=HabitRead)
 def update_habit(habit_id: int, habit_update: HabitUpdate, db: db_dependency):
-    habit = db.query(Habit).filter(Habit.id == habit_id).first()
-
-    if habit is None:
-        raise HTTPException(status_code=404, detail="Habit Not Found")
-    
-    if habit_update.title is not None:
-        habit.title = habit_update.title 
-    
-    if habit_update.description is not None:
-        habit.description = habit_update.description 
-
-    db.commit()
-    db.refresh(habit)
-
-    return habit
+    return habit_service.update_habit(habit_id, habit_update, db)
 
 @router.delete("/{habit_id}", status_code=204)
 def delete_habit(habit_id: int, db: db_dependency):
-    habit = db.query(Habit).filter(Habit.id == habit_id).first()
-
-    if habit is None:
-        raise HTTPException(status_code=404, detail="Habit not found")
-    
-    db.delete(habit)
-    db.commit()
+    return habit_service.delete_habit(habit_id, db)
---
+++ app/services/habit_service.py
@@ -0,0 +1,56 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database.session import get_db 
+from app.database.models import Habit 
+from app.schemas.habit import HabitCreate, HabitRead, HabitUpdate
+
+def get_habits(db: Session):
+    return db.query(Habit).order_by(Habit.created_at, Habit.id).all()
+
+def get_habit(habit_id: int, db: Session):
+    habit = db.query(Habit).filter(Habit.id == habit_id).first()
+
+    if habit is None:
+        raise HTTPException(status_code=404, detail="Habit not found")
+    
+    return habit
+
+def create_habit(habit, db):
+    new_habit = Habit(
+        title = habit.title,
+        description = habit.description
+    )
+
+    db.add(new_habit)
+    db.commit()
+    db.refresh(new_habit)
+
+    return new_habit
+
+def update_habit(habit_id, habit_update, db):
+    habit = db.query(Habit).filter(Habit.id == habit_id).first()
+
+    if habit is None:
+        raise HTTPException(status_code=404, detail="Habit Not Found")
+    
+    if habit_update.title is not None:
+        habit.title = habit_update.title 
+    
+    if habit_update.description is not None:
+        habit.description = habit_update.description 
+
+    db.commit()
+    db.refresh(habit)
+
+    return habit
+
+def delete_habit(habit_id, db):
+    habit = db.query(Habit).filter(Habit.id == habit_id).first()
+
+    if habit is None:
+        raise HTTPException(status_code=404, detail="Habit not found")
+    
+    db.delete(habit)
+    db.commit()
